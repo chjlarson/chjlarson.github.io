73f239ce52711f68b09877e973fb75a47d535189
Christopher Larson <chjlarson@gmail.com>
2017-11-20 20:02:22 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -14,11 +14,9 @@ $(document).ready(function(){
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 900, function(){
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
-    } // End if
+    } 
   });
 
   $(window).scroll(function() {
